be459b2d3e4401b2b6e6346bd855326beb7fcbcd
tYohannes <teklegy@gmail.com>
2022-03-08 13:06:18 +0300

    Fetch data from covid-19 api using createAsyncThunk action creator function

--- src/store/covidStats.js
+++ src/store/covidStats.js
@@ -1,4 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchCovidStats = createAsyncThunk(
+  'covidStats/fetchStats',
+  async (date = '2022-03-06', thunkApi) => {
+    const { dispatch } = thunkApi;
+    const { data } = await axios.get(`https://api.covid19tracking.narrativa.com/api/${date}`);
+    console.log(data);
+    console.log(dispatch);
+    // const rockets = data.map((rocket) => (
+    //   {
+    //     id: rocket.id,
+    //     rocketName: rocket.rocket_name,
+    //     description: rocket.description,
+    //     rocketImage: rocket.flickr_images[0],
+    //   }
+    // ));
+    // dispatch({
+    //   type: rocketsFetched.type,
+    //   payload: rockets,
+    // });
+  },
+);
 
 const slice = createSlice({
   name: 'covidStats',
