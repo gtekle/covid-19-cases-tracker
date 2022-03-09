ee279046a31506f74ea42085bae280324628ffad
tYohannes <teklegy@gmail.com>
2022-03-10 01:13:16 +0300

    Add draft styling to Countries List

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { DateTime } from 'luxon';
+import { FaChevronLeft } from 'react-icons/fa';
+import WORLD_MAP from '../assets/img/world-map.png';
 
 import { fetchCovidStats } from '../store/covidStats';
 import Country from './Country';
@@ -10,28 +12,59 @@ const CountriesList = () => {
   const today = DateTime.now().toISODate();
   const dispatch = useDispatch();
   const { casesByCountry, totalCases } = useSelector((state) => state);
+  let alternatingBackgroundColor = 'default_color';
   useEffect(async () => {
     if (Object.keys(casesByCountry).length === 0) dispatch(fetchCovidStats({ date: today }));
   }, []);
   return (
-    <div>
-      <p>CountriesList</p>
-      <p>
-        Todate Confirmed Cases:
-        {totalCases.today_confirmed}
-      </p>
-      <p>
-        Todate Deaths:
-        {totalCases.today_deaths}
-      </p>
-      <ul>
+    <div className="countries_list_container">
+      <div className="countries_list_header">
+        <div className="countries_list_header_back">
+          <FaChevronLeft />
+          <span>{today}</span>
+        </div>
+        <span>All Stats</span>
+      </div>
+      <div className="countries_list_hero">
+        <img src={WORLD_MAP} alt="world map" />
+        <div className="hero_text">
+          <h1>Worldwide</h1>
+          <span>{totalCases.today_confirmed.toLocaleString('en-US')}</span>
+        </div>
+      </div>
+      <div className="all_stats_divider">
+        <span>all stats</span>
+      </div>
+      <div className="all_stats_detail">
+        <p>
+          Todate Confirmed Cases:
+          {totalCases.today_confirmed.toLocaleString('en-US')}
+        </p>
+        <p>
+          Todate Deaths:
+          {totalCases.today_deaths.toLocaleString('en-US')}
+        </p>
+      </div>
+      <div className="stats_by_country_divider">
+        <span>stats by country</span>
+      </div>
+      <ul className="countries_list">
         {
           Object.keys(casesByCountry).map(
-            (country) => (
-              <Link key={country} to={country}>
-                <Country country={casesByCountry[country]} />
-              </Link>
-            ),
+            (country, idx) => {
+              if ((idx + 1) % 2 === 0) {
+                if (alternatingBackgroundColor === 'default_color') {
+                  alternatingBackgroundColor = 'other_color';
+                } else {
+                  alternatingBackgroundColor = 'default_color';
+                }
+              }
+              return (
+                <Link key={country} to={country} className={alternatingBackgroundColor}>
+                  <Country country={casesByCountry[country]} />
+                </Link>
+              );
+            },
           )
         }
       </ul>
